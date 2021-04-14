20fbb1eb8b6775eb6ff437b1057c6841aa562a4e
Souradeep Nanda <Ghost---Shadow@users.noreply.github.com>
2021-04-14 21:00:02 +0530

    Remove pizzas route

--- routes/index.go
+++ routes/index.go
@@ -3,9 +3,6 @@ package routes
 import fiber "github.com/gofiber/fiber/v2"
 
 func RegisterRoutes(app *fiber.App) {
-	app.Get("/pizza", GetPizza)
-	app.Post("/pizza", PostPizza)
-
 	app.Get("/warehouse/:id", GetWarehouse)
 	app.Post("/warehouse", PostWarehouse)
 
